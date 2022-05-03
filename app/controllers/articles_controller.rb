1bd1acd4356db2e8f39448ea678689e7f12da2dd
Mikegit1988 <mikenerly@gmail.com>
2022-05-03 03:53:39 +0000

    Updated index.html.erb with a new line

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,12 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
       
    end
+   
+   #define index#action for root of articles
+   def index
+       
+       @articles = Article.all
+       
+   end
     
 end
