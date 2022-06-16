3a87c0f716d94893e1ba191d78c4eabe6f1dc85b
Mikegit1988 <mikenerly@gmail.com>
2022-06-16 04:49:57 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-    
+    #add validations for user and email
+    validates :username, presence: true, 
+                         uniqueness: { case_sensitive: false }, 
+                         length: { minimum: 3, maximum: 25 }
+    #adding regular expression that will match pattern
+    #as constaint variable name
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, 
+                         uniqueness: { case_sensitive: false }, 
+                         length: { maximum: 105 },
+                         format: { with: VALID_EMAIL_REGEX }
+    #to test regular expression go to rebular.com
 end
