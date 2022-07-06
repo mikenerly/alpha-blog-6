ba5ed18a2ddbc5836e7a7f2b678a70727592c8dd
Mikegit1988 <mikenerly@gmail.com>
2022-07-06 14:49:04 +0000

    Add user login and logout form

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     #post 'users', to: 'users#create' #one route
     resources :users, except: [:new]
-    
+    #add route for login page
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
 end
